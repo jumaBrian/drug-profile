7df4c5cadf9fab0f1a10295cb8db2e2ed07ca212
jumaBrian <njengabrianj@gmail.com>
2022-05-25 00:58:34 +0300

    ft: Include data binding

--- app/src/main/java/com/brayo/drugprofile/MainActivity.java
+++ app/src/main/java/com/brayo/drugprofile/MainActivity.java
@@ -1,35 +1,35 @@
 package com.brayo.drugprofile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.brayo.drugprofile.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText enterDrugComposition;
-    private TextView drugComposition;
-
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        enterDrugComposition = findViewById(R.id.enter_composition);
-        drugComposition = findViewById(R.id.drug_composition);
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.submitButton.setOnClickListener(this::addDrugComposition);
     }
 
     public void addDrugComposition(View view) {
-        drugComposition.setText(String.format("Ingredients: %s", enterDrugComposition.getText().toString().trim()));
-        drugComposition.setVisibility(View.VISIBLE);
+        binding.drugComposition.setText(String.format("Ingredients: %s",
+                binding.enterComposition.getText().toString().trim()));
+        binding.invalidateAll();
+        binding.drugComposition.setVisibility(View.VISIBLE);
 
         // hide keyboard after input
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        assert inputMethodManager != null;
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
